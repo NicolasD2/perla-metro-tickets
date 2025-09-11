b50ecafa6eef22b003839ca0531a6901cfe834bf
NicolasD2 <nicolas.diaz03@alumnos.ucn.cl>
2025-09-10 23:22:15 -0400

    fix: ticket service, add created and updated

--- src/tickets/tickets.service.ts
+++ src/tickets/tickets.service.ts
@@ -7,7 +7,16 @@ import { CreateTicketDto } from "./Dto/create-ticket.dto";
 export class TicketsService {
   constructor(private readonly ticketsRepository: TicketsRepository) {}
    create(createTicketDto: CreateTicketDto): Ticket {
-    const ticket: Ticket = { id: Date.now().toString(), ...createTicketDto };
+    const now = new Date().toISOString();
+    const ticket: Ticket = { id: Date.now().toString(),
+        passengerId: createTicketDto.passengerId,
+        date: createTicketDto.date,
+        type: createTicketDto.type,
+        status: createTicketDto.status,
+        paid: createTicketDto.paid,
+        createdAt: new Date(now),
+        updatedAt: new Date(now)
+    };
     return this.ticketsRepository.create(ticket);
   }
    findAll(): Ticket[] {

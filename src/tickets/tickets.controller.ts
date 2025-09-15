ed062bffdce4a2e2c4cc3aeb5b9a15142a3d7271
NicolasD2 <nicolas.diaz03@alumnos.ucn.cl>
2025-09-15 05:48:34 -0400

    refactor: update tickets controller to async methods and promise return

--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -9,26 +9,25 @@ export class TicketsController {
   constructor(private readonly ticketsService: TicketsService) {}
 
   @Post()
-  create(@Body() createTicketDto: CreateTicketDto): Ticket {
+  create(@Body() createTicketDto: CreateTicketDto): Promise<Ticket> {
     return this.ticketsService.create(createTicketDto);
   }
 
   @Get()
-  findAll(@Query('admin') admin: string): Ticket[] {
+  findAll(@Query('admin') admin: string): Promise<Ticket[]> {
     return this.ticketsService.findAll(admin === 'true');
   }
 
   @Get(':id')
-  findById(@Param('id') id: string): Partial <Ticket>{
+  findById(@Param('id') id: string): Promise<Partial<Ticket>> {
     return this.ticketsService.findById(id);
   }
   @Patch(':id')
-  update(@Param('id') id: string, @Body() dto: UpdateTicketDto): Ticket {
+  update(@Param('id') id: string, @Body() dto: UpdateTicketDto): Promise<Ticket> {
     return this.ticketsService.update(id, dto);
   }
   @Delete(':id')
-  softDelete(@Param('id') id: string, @Query('admin') admin : string): boolean{
-     
+  softDelete(@Param('id') id: string, @Query('admin') admin : string): Promise<boolean>{
     return this.ticketsService.softDelete(id, admin==='true');
   }
 }

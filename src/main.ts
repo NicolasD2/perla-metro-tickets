204070345dd12b69e146dcee464eacacddcda763
NicolasD2 <nicolas.diaz03@alumnos.ucn.cl>
2025-09-27 01:12:54 -0400

    feat: modify console logs for render

--- src/main.ts
+++ src/main.ts
@@ -22,14 +22,13 @@ async function bootstrap() {
   const port = process.env.PORT || 5050;
   await app.listen(port, '0.0.0.0');
 
-  console.log('Backend API - Sistema de Tickets ejecutandose en el puerto ${port}');
-  console.log(` URL base: ${process.env.NODE_ENV === 'production' ? 'https://tu-app.onrender.com' : 'http://localhost:' + port}/api`);
-  console.log('Endpoints disponibles:');
-  console.log('GET /api/tickets/obtener?admin=true|false - Listar todos los tickets')
-  console.log('POST /api/tickets/crear - Crear un nuevo ticket')
-  console.log('GET /api/tickets/buscar/:id - Obtener un ticket por ID')
-  console.log('PATCH /api/tickets/actualizar/:id - Actualizar un ticket por ID')
-  console.log('DELETE /api/tickets/eliminar/:id?admin=true|false - Eliminar (soft delete) un ticket por ID')
+  console.log(`Backend API - Sistema de Tickets ejecutandose en el puerto ${port}`);
+  console.log(`Endpoints disponibles:`);
+  console.log(`GET /api/tickets/obtener?admin=true|false - Listar todos los tickets`);
+  console.log(`POST /api/tickets/crear - Crear un nuevo ticket`);
+  console.log(`GET /api/tickets/buscar/:id - Obtener un ticket por ID`);
+  console.log(`PATCH /api/tickets/actualizar/:id - Actualizar un ticket por ID`);
+  console.log(`DELETE /api/tickets/eliminar/:id?admin=true|false - Eliminar (soft delete) un ticket por ID`);
 }
 
 void bootstrap();

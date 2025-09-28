8aee773d1b0f38b1aa25534ece2f77a0ccea99fa
NicolasD2 <nicolas.diaz03@alumnos.ucn.cl>
2025-09-28 17:49:01 -0400

    fix: format imports in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,10 @@ import { TicketsModule } from './tickets/tickets.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://perla_admin:2ak13p02@perla-metro-ticket-serv.jq2wzva.mongodb.net/'),   TicketsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://perla_admin:2ak13p02@perla-metro-ticket-serv.jq2wzva.mongodb.net/perla-metro-tickets'),
+    TicketsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
